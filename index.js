1f8c5c030e9044188d9d58b94a29c2b8ae810c18
JounQin <admin@1stg.me>
2017-05-31 10:35:26 +0800

    big refactor and improvement of performance

--- index.js
+++ index.js
@@ -1,51 +1,95 @@
-const NativeModule = require('module')
 const vm = require('vm')
 const path = require('path')
+const NativeModule = require('module')
 
-function createContext(context, CONTEXT_KEY) {
+const resolve = require('resolve')
+
+const _toString = Object.prototype.toString
+
+const isPlainObject = obj => _toString.call(obj) === '[object Object]'
+
+function createSandbox(context, CONTEXT_KEY) {
   const sandbox = {
     Buffer,
-    clearImmediate,
-    clearInterval,
-    clearTimeout,
-    setImmediate,
-    setInterval,
-    setTimeout,
     console,
     process,
+    setTimeout,
+    setInterval,
+    setImmediate,
+    clearTimeout,
+    clearInterval,
+    clearImmediate,
     [CONTEXT_KEY]: context
   }
   sandbox.global = sandbox
   return sandbox
 }
 
-function evaluateModule(filename, files, context, evaluatedModules) {
-  if (evaluatedModules[filename]) {
-    return evaluatedModules[filename]
-  }
+function compileModule(files, basedir, runInNewContext) {
+  const compiledScripts = {}
+  const resolvedModules = {}
 
-  const code = files[filename]
-  const wrapper = NativeModule.wrap(code)
-  const compiledWrapper = vm.runInNewContext(wrapper, context, {
-    filename,
-    displayErrors: true
-  })
-  const m = {exports: {}}
-  const r = file => {
-    file = path.join('.', file)
-    if (files[file]) {
-      return evaluateModule(file, files, context, evaluatedModules)
-    } else {
-      return require(file)
+  function getCompiledScript(filename) {
+    if (compiledScripts[filename]) {
+      return compiledScripts[filename]
     }
+    const code = files[filename]
+    const wrapper = NativeModule.wrap(code)
+    const script = new vm.Script(wrapper, {
+      filename,
+      displayErrors: true
+    })
+    compiledScripts[filename] = script
+    return script
   }
-  compiledWrapper.call(m.exports, m.exports, r, m)
 
-  const res = Object.prototype.hasOwnProperty.call(m.exports, 'default') ?
-    m.exports.default :
-    m.exports
-  evaluatedModules[filename] = res
-  return res
+  function evaluateModule(filename, sandbox, evaluatedFiles = {}) {
+    if (evaluatedFiles[filename]) {
+      return evaluatedFiles[filename]
+    }
+
+    const script = getCompiledScript(filename)
+    const compiledWrapper = runInNewContext === false
+      ? script.runInThisContext()
+      : script.runInNewContext(sandbox)
+    const m = {exports: {}}
+    const r = file => {
+      file = path.join('.', file)
+      if (files[file]) {
+        return evaluateModule(file, sandbox, evaluatedFiles)
+      } else if (basedir) {
+        return require(
+          resolvedModules[file] ||
+          (resolvedModules[file] = resolve.sync(file, {basedir}))
+        )
+      } else {
+        return require(file)
+      }
+    }
+    compiledWrapper.call(m.exports, m.exports, r, m)
+
+    const res = Object.prototype.hasOwnProperty.call(m.exports, 'default')
+      ? m.exports.default
+      : m.exports
+    evaluatedFiles[filename] = res
+    return res
+  }
+
+  return evaluateModule
+}
+
+function deepClone(val) {
+  if (isPlainObject(val)) {
+    const res = {}
+    for (const key in val) {
+      res[key] = deepClone(val[key])
+    }
+    return res
+  } else if (Array.isArray(val)) {
+    return val.slice()
+  } else {
+    return val
+  }
 }
 
 let DEFAULT_CONTEXT_KEY
@@ -57,13 +101,55 @@ try {
   try {
     require('vue-style-loader')
     DEFAULT_CONTEXT_KEY = '__VUE_SSR_CONTEXT__'
-  } catch (e) {}
+  } catch (e) {
+  }
 }
 
-module.exports = function runInVm({entry, files}, _context = {}, CONTEXT_KEY = DEFAULT_CONTEXT_KEY) {
-  return new Promise(resolve => {
-    const context = createContext(_context, CONTEXT_KEY)
-    const res = evaluateModule(entry, files, context, {})
-    resolve(typeof res === 'function' ? res(_context) : res)
-  })
+function createBundleRunner(entry, files, basedir, runInNewContext, CONTEXT_KEY = DEFAULT_CONTEXT_KEY) {
+  const evaluate = compileModule(files, basedir, runInNewContext)
+  if (runInNewContext !== false && runInNewContext !== 'once') {
+    // new context mode: creates a fresh context and re-evaluate the bundle
+    // on each render. Ensures entire application state is fresh for each
+    // render, but incurs extra evaluation cost.
+    return (userContext = {}) => new Promise(resolve => {
+      userContext._registeredComponents = new Set()
+      const res = evaluate(entry, createSandbox(userContext, CONTEXT_KEY))
+      resolve(typeof res === 'function' ? res(userContext) : res)
+    })
+  } else {
+    // direct mode: instead of re-evaluating the whole bundle on
+    // each render, it simply calls the exported function. This avoids the
+    // module evaluation costs but requires the source code to be structured
+    // slightly differently.
+    let runner // lazy creation so that errors can be caught by user
+    let initialContext
+    return (userContext = {}) => new Promise(resolve => {
+      if (!runner) {
+        const sandbox = runInNewContext === 'once'
+          ? createSandbox(null, CONTEXT_KEY)
+          : global
+        // the initial context is only used for collecting possible non-component
+        // styles injected by vue-style-loader.
+        initialContext = sandbox[CONTEXT_KEY] = {}
+        runner = evaluate(entry, sandbox)
+        // On subsequent renders, CONTEXT_KEY will not be available
+        // to prevent cross-request pollution.
+        delete sandbox[CONTEXT_KEY]
+        if (typeof runner !== 'function') {
+          throw new Error(
+            'bundle export should be a function when using ' +
+            '{ runInNewContext: false }.'
+          )
+        }
+      }
+      userContext._registeredComponents = new Set()
+      // react/vue-style-loader styles imported outside of component lifecycle hooks
+      if (initialContext._styles) {
+        userContext._styles = deepClone(initialContext._styles)
+      }
+      resolve(runner(userContext))
+    })
+  }
 }
+
+module.exports = ({bundle: {entry, files}, basedir, runInNewContext}) => createBundleRunner(entry, files, basedir, runInNewContext)
