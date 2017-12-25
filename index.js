686cb582c6e17e7c477402298a31eb3c9c632fed
Jiaren Liu <jrliu@alauda.io>
2017-12-25 21:27:12 +0800

    bump dependency, update from vue core

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function createSandbox(context, CONTEXT_KEY) {
     clearTimeout,
     clearInterval,
     clearImmediate,
-    [CONTEXT_KEY]: context
+    [CONTEXT_KEY]: context,
   }
   sandbox.global = sandbox
   return sandbox
@@ -37,7 +37,7 @@ function compileModule(files, basedir, runInNewContext) {
     const wrapper = NativeModule.wrap(code)
     const script = new vm.Script(wrapper, {
       filename,
-      displayErrors: true
+      displayErrors: true,
     })
     compiledScripts[filename] = script
     return script
@@ -49,19 +49,18 @@ function compileModule(files, basedir, runInNewContext) {
     }
 
     const script = getCompiledScript(filename)
-    const compiledWrapper = runInNewContext === false
-      ? script.runInThisContext()
-      : script.runInNewContext(sandbox)
-    const m = {exports: {}}
+    const compiledWrapper =
+      runInNewContext === false
+        ? script.runInThisContext()
+        : script.runInNewContext(sandbox)
+    const m = { exports: {} }
     const r = file => {
-      file = path.join('.', file)
+      file = path.posix.join('.', file)
       if (files[file]) {
         return evaluateModule(file, sandbox, evaluatedFiles)
       } else if (basedir) {
-        return require(
-          resolvedModules[file] ||
-          (resolvedModules[file] = resolve.sync(file, {basedir}))
-        )
+        return require(resolvedModules[file] ||
+          (resolvedModules[file] = resolve.sync(file, { basedir })))
       } else {
         return require(file)
       }
@@ -74,7 +73,6 @@ function compileModule(files, basedir, runInNewContext) {
     evaluatedFiles[filename] = res
     return res
   }
-
   return evaluateModule
 }
 
@@ -101,21 +99,27 @@ try {
   try {
     require('vue-style-loader')
     DEFAULT_CONTEXT_KEY = '__VUE_SSR_CONTEXT__'
-  } catch (e) {
-  }
+  } catch (e) {}
 }
 
-function createBundleRunner(entry, files, basedir, runInNewContext, CONTEXT_KEY) {
+function createBundleRunner(
+  entry,
+  files,
+  basedir,
+  runInNewContext,
+  CONTEXT_KEY,
+) {
   const evaluate = compileModule(files, basedir, runInNewContext)
   if (runInNewContext !== false && runInNewContext !== 'once') {
     // new context mode: creates a fresh context and re-evaluate the bundle
     // on each render. Ensures entire application state is fresh for each
     // render, but incurs extra evaluation cost.
-    return (userContext = {}) => new Promise(resolve => {
-      userContext._registeredComponents = new Set()
-      const res = evaluate(entry, createSandbox(userContext, CONTEXT_KEY))
-      resolve(typeof res === 'function' ? res(userContext) : res)
-    })
+    return (userContext = {}) =>
+      new Promise(resolve => {
+        userContext._registeredComponents = new Set()
+        const res = evaluate(entry, createSandbox(userContext, CONTEXT_KEY))
+        resolve(typeof res === 'function' ? res(userContext) : res)
+      })
   } else {
     // direct mode: instead of re-evaluating the whole bundle on
     // each render, it simply calls the exported function. This avoids the
@@ -123,34 +127,50 @@ function createBundleRunner(entry, files, basedir, runInNewContext, CONTEXT_KEY)
     // slightly differently.
     let runner // lazy creation so that errors can be caught by user
     let initialContext
-    return (userContext = {}) => new Promise(resolve => {
-      if (!runner) {
-        const sandbox = runInNewContext === 'once'
-          ? createSandbox(null, CONTEXT_KEY)
-          : global
-        // the initial context is only used for collecting possible non-component
-        // styles injected by vue-style-loader.
-        initialContext = sandbox[CONTEXT_KEY] = {}
-        runner = evaluate(entry, sandbox)
-        // On subsequent renders, CONTEXT_KEY will not be available
-        // to prevent cross-request pollution.
-        delete sandbox[CONTEXT_KEY]
-        if (typeof runner !== 'function') {
-          throw new Error(
-            'bundle export should be a function when using ' +
-            '{ runInNewContext: false }.'
-          )
+    return (userContext = {}) =>
+      new Promise(resolve => {
+        if (!runner) {
+          const sandbox = runInNewContext === 'once' ? createSandbox() : global
+          // the initial context is only used for collecting possible non-component
+          // styles injected by react/vue-style-loader.
+          initialContext = sandbox[CONTEXT_KEY] = {}
+          runner = evaluate(entry, sandbox)
+          // On subsequent renders, CONTEXT_KEY will not be available
+          // to prevent cross-request pollution.
+          delete sandbox[CONTEXT_KEY]
+          if (typeof runner !== 'function') {
+            throw new Error(
+              'bundle export should be a function when using ' +
+                '{ runInNewContext: false }.',
+            )
+          }
         }
-      }
-      userContext._registeredComponents = new Set()
-      // react/vue-style-loader styles imported outside of component lifecycle hooks
-      if (initialContext._styles) {
-        userContext._styles = deepClone(initialContext._styles)
-      }
-      resolve(runner(userContext))
-    })
+
+        userContext._registeredComponents = new Set()
+
+        // react/vue-style-loader styles imported outside of component lifecycle hooks
+        if (initialContext._styles) {
+          userContext._styles = deepClone(initialContext._styles)
+          // #6353 ensure "styles" is exposed even if no styles are injected
+          // in component lifecycles.
+          // the renderStyles fn is exposed by react/vue-style-loader
+          const renderStyles = initialContext._renderStyles
+          if (renderStyles) {
+            Object.defineProperty(userContext, 'styles', {
+              enumerable: true,
+              get() {
+                return renderStyles(userContext._styles)
+              },
+            })
+          }
+        }
+
+        resolve(runner(userContext))
+      })
   }
 }
 
-module.exports = ({bundle: {entry, files}, basedir, runInNewContext}, CONTEXT_KEY = DEFAULT_CONTEXT_KEY) =>
-  createBundleRunner(entry, files, basedir, runInNewContext, CONTEXT_KEY)
+module.exports = (
+  { bundle: { entry, files }, basedir, runInNewContext },
+  CONTEXT_KEY = DEFAULT_CONTEXT_KEY,
+) => createBundleRunner(entry, files, basedir, runInNewContext, CONTEXT_KEY)
